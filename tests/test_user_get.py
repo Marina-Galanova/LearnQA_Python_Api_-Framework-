d039e1c33724833d8437ab48f8c89cecaeb54548
Галанова Марина <mmgalanova@fintechiq.ru>
2023-11-04 08:52:16 +0700

    Ex16 add test for test_user_get.py

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -32,3 +32,23 @@ class TestUserGet(BaseCase):
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
 
+    def test_get_user_details_auth_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response3 = MyRequests.post("/user/login", data=data)
+        auth_sid = self.get_cookie(response3, "auth_sid")
+        token = self.get_headers(response3, "x-csrf-token")
+
+        response4 = MyRequests.get(
+            "/user/1",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response4, "username")
+        Assertions.assert_json_has_not_key(response4, "email")
+        Assertions.assert_json_has_not_key(response4, "firstName")
+        Assertions.assert_json_has_not_key(response4, "lastName")
